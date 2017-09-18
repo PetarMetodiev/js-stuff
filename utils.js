c1fde455ba8ba5447eca7e06d0e34c3d1550672a
Petar Metodiev <petar.metodiev@gmail.com>
2017-09-18 22:39:22 +0300

    Minor changes
    Source(url) of curry function.

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 const pipe = (...fns) => x => fns.reduce((y, f) => f(y), x);
 const compose = (...fns) => x => fns.reduceRight((y, f) => f(y), x);
-// const curry = fn => (...args) => fn.bind(null, ...args);
+// https://github.com/learn-javascript-courses/es6-curry#add3-partially-applied--autocurried
 const curry = (
 		f
 		, arr = []
@@ -9,6 +9,7 @@ const curry = (
 		...args
 	) =>
 	(a => a.length === f.length ? f(...a) : curry(f, a))([...arr, ...args]);
+
 const head = ([x]) => x;
 const tail = ([x, ...xs]) => xs;
 
