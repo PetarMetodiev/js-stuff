e88eaec2e39f8b96ca2441d5f1b7e5166201e046
Petar Metodiev <petar.metodiev@gmail.com>
2017-10-22 23:03:07 +0300

    Add filter and swap(using array#map)
    Fix length to work with 0 values.
    Formatting.

--- utils.js
+++ utils.js
@@ -16,7 +16,7 @@ const def = (x) => typeof x !== 'undefined';
 const undef = (x) => !def(x);
 const copy = (arr) => [...arr];
 
-const length = (len = 0, [x, ...xs]) => x ?
+const length = (len = 0, [x, ...xs]) => def(x) ?
 	length(len + 1, xs) :
 	len;
 
@@ -27,8 +27,7 @@ const first = (n = 1, [x, ...xs]) => def(x) && n > 0 ? [x, ...first(n - 1, xs)] 
 const last = (n = 1, xs) => reverse(first(reverse(xs), n));
 
 const insertAt = (pos, el, curr = 0, [x, ...xs]) => def(x) ?
-	curr === pos ? [el, x, ...insertAt(pos, el, curr + 1, xs)] : [x, ...insertAt(pos, el, curr + 1, xs)] :
-	curr <= pos ? [el] : []
+	curr === pos ? [el, x, ...insertAt(pos, el, curr + 1, xs)] : [x, ...insertAt(pos, el, curr + 1, xs)] : curr <= pos ? [el] : [];
 
 const isArray = (x) => Array.isArray(x);
 
@@ -36,6 +35,21 @@ const flatten = ([x, ...xs]) => def(x) ?
 	isArray(x) ? [...flatten(x), ...flatten(xs)] : [x, ...flatten(xs)] : [];
 
 const map = (fn, [x, ...xs]) => def(x) ? [fn(x), ...map(fn, xs)] : [];
+
+// https://www.reddit.com/r/javascript/comments/4ppifp/better_functional_way_to_swap_array_values/
+const swap = (i, j, a) => a.map((v, k) => {
+	switch (k) {
+	case i:
+		return a[j];
+	case j:
+		return a[i];
+	default:
+		return v;
+	}
+});
+
+const filter = (fn, [x, ...xs]) => def(x) ?
+	fn(x) ? [x, ...filter(fn,xs)] : [...filter(fn, xs)] : [];
 
 const factorial = (n, acc = 1) => n < 2 ? acc : factorial(n - 1, n * acc);
 
@@ -56,5 +70,7 @@ module.exports = {
 	, isArray
 	, flatten
 	, map
+	, swap
+	, filter
 	, factorial
 };
