074508ae51274643a0fe56804117151fd7e1add4
Petar Metodiev <petar.metodiev@gmail.com>
2017-11-22 22:19:28 +0200

    Minor changes

--- functional-exercises/composing.js
+++ functional-exercises/composing.js
@@ -112,7 +112,7 @@ console.log(averageDollarValue3(CARS));
 // ============
 // Write a function: sanitizeNames() using compose that returns a list of lowercase and underscored car's names: e.g: sanitizeNames([{name: 'Ferrari FF', horsepower: 660, dollar_value: 700000, in_stock: true}]) //=> ['ferrari_ff'].
 
-var _underscore = _.replace(/\W+/g, '_'); //<-- leave this alone and use to sanitize
+const _underscore = _.replace(/\W+/g, '_'); //<-- leave this alone and use to sanitize
 
 var sanitizeNames = undefined;
 
