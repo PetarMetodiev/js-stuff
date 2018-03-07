58c649ae5332a618bd2518453595d432cbf59ef2
Petar Metodiev <petar.metodiev@gmail.com>
2018-03-07 23:59:02 +0200

    First functor

---
+++ functional-exercises/functors-and-monads.js
@@ -0,0 +1,16 @@
+// https://drboolean.gitbooks.io/mostly-adequate-guide-old/content/ch8.html
+const Container = function (x) {
+	this.__value = x;
+}
+
+Container.of = function (x) {
+	return new Container(x);
+}
+
+Container.prototype.map = function(xf) {
+	return Container.of(xf(this.__value));
+}
+
+module.exports = {
+	Container
+};
